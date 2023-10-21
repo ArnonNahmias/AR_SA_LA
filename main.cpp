32edbd0d7a5c86c35874ec8b00cf6ba1756cad32
lautiod <dantelojeda05@gmail.com>
2023-10-21 17:33:15 -0300

    Inventario

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "string.h"
+using namespace std;
 
-int main() {
-    std::cout << "puto el que lee!" << std::endl;
+void chopCSV(string filename, int lines);
+
+void exploreCSV(string filename);
+
+void exploreHeaders(string fileName){
+    fstream fin;
+    fin.open("./" + fileName, ios::in);
+
+    string headers, header;
+    getline(fin, header);
+    stringstream s(headers);
+    while (getline(s, header, ','))
+    {
+        cout<< header << endl;
+    }
+}
+
+int main(int argc, char **argv)
+{
+    cout << "Cantidad de argumentos: " << argc << endl;
+    for (int i=0 ; i < argc; i++)
+    {
+        cout << "Argumento " << i << ": " <<argv[i] << endl;
+
+        if (strcmp(argv[i], ".file") ==0){
+            cout << "Nombre del archivo: " << argv[i+1] << endl;
+            exploreHeaders(argv[i+1]);
+            break;
+        }
+    }
     return 0;
 }
+
