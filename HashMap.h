3d3e7b344d9d2b268bc298e418527459ac665611
lautiod <127853930+lautiod@users.noreply.github.com>
2023-11-09 00:20:32 -0300

    Update HashMap.h

--- HashMap.h
+++ HashMap.h
@@ -17,15 +17,23 @@ private:
     size_t tamano;
 
     size_t hashFunc(const K& clave) const {
-        // Asumiendo una funcion hash genérica para K.
         return hash<K>()(clave) % capacidad;
     }
 
 public:
+    /**
+     * @brief Constructor de HashMap que inicializa la capacidad de la tabla hash.
+     * @param capacidad La capacidad inicial de la tabla hash (por defecto 1000).
+     */
     HashMap(size_t capacidad = 1000) : capacidad(capacidad), tamano(0) {
         tabla.resize(capacidad);
     }
 
+    /**
+     * @brief Inserta un par clave-valor en la tabla hash.
+     * @param clave La clave del elemento a insertar.
+     * @param valor El valor del elemento a insertar.
+     */
     void insertar(const K& clave, const V& valor) {
         size_t indice = hashFunc(clave);
         auto& lista = tabla[indice];
@@ -39,7 +47,11 @@ public:
             ++tamano;
         }
     }
-
+    /**
+     * @brief Verifica si una clave está presente en la tabla hash.
+     * @param clave La clave que se quiere verificar.
+     * @return true si la clave está presente, false en caso contrario.
+     */
     bool contieneClave(const K& clave) const {
         size_t indice = hashFunc(clave);
         const auto& lista = tabla[indice];
@@ -48,6 +60,12 @@ public:
         });
     }
 
+    /**
+     * @brief Obtiene el valor asociado con una clave específica.
+     * @param clave La clave cuyo valor se desea obtener.
+     * @return El valor asociado con la clave proporcionada.
+     * @throw runtime_error si la clave no se encuentra en la tabla hash.
+     */
     V obtener(const K& clave) const {
         size_t indice = hashFunc(clave);
         const auto& lista = tabla[indice];
@@ -60,7 +78,10 @@ public:
             throw runtime_error("Clave no encontrada");
         }
     }
-
+    /**
+     * @brief Elimina el par clave-valor asociado con una clave específica.
+     * @param clave La clave del elemento a eliminar.
+     */
     void eliminar(const K& clave) {
         size_t indice = hashFunc(clave);
         auto& lista = tabla[indice];
@@ -72,11 +93,19 @@ public:
             --tamano;
         }
     }
-
-    size_t obtenerTamano() const {
+    /**
+     * @brief Obtiene el número de elementos actualmente en la tabla hash.
+     * @return El tamaño actual de la tabla hash.
+     */
+    size_t getTamano() const {
         return tamano;
     }
 
+    /**
+     * @brief Obtiene todos los artículos en la tabla hash y los almacena en un arreglo.
+     * @param count Referencia a un entero donde se almacena el número de artículos.
+     * @return Puntero a un arreglo dinámico que contiene todos los artículos.
+     */
     Articulo* obtenerTodosLosArticulos(int& count) const {
         count = tamano; // Establecemos el conteo de articulos.
         if (count == 0) return nullptr; // Si no hay articulos, retornamos nullptr.
