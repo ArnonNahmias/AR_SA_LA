7e51875e8574a9d46ff82e34de6648896815c67b
lautiod <127853930+lautiod@users.noreply.github.com>
2023-11-08 21:26:08 -0300

    Update Articulo.h

--- Articulo.h
+++ Articulo.h
@@ -1,7 +1,3 @@
-//
-// Created by dante on 24/10/2023.
-//
-
 #ifndef PROYECTO_ARTICULO_H
 #define PROYECTO_ARTICULO_H
 
@@ -10,42 +6,106 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
+// Clase que representa un artículo en el inventario.
 class Articulo {
 private:
-    string grupo;
-    string codigoDeBarras;
-    string nombre;
-    vector<string> depositos;
+    string grupo; // Grupo o categoría del artículo.
+    string codigoDeBarras; // Código único del artículo.
+    string nombre; // Nombre descriptivo del artículo.
+    vector<int> depositos; // Lista de depósitos donde se encuentra el artículo.
+
 public:
-    Articulo() {}
+    // Constructor por defecto.
+    Articulo() : grupo(""), codigoDeBarras(""), nombre("") {}
+
+    // Constructor que inicializa un artículo con grupo, código de barras y nombre.
     Articulo(const string &grupo, const string &codigoDeBarras, const string &nombre) :
             grupo(grupo), codigoDeBarras(codigoDeBarras), nombre(nombre) {}
 
-    void agregarDeposito(const std::string &deposito) {
+    // Agrega un nuevo depósito a la lista de depósitos del artículo.
+    void agregarDeposito(const int &deposito) {
         depositos.push_back(deposito);
     }
 
+    // Método para obtener el stock total del artículo.
+    int getStockTotal() const {
+        int total = 0;
+        for (int cantidad : depositos) {
+            total += cantidad;
+        }
+        return total;
+    }
+
+    // Imprime la información del artículo en consola.
     void imprimir() {
         int cantDepositos = depositos.size();
-        cout << "> Grupo: " << grupo << endl;
-        cout << "> Codigo de Barras: " << codigoDeBarras << endl;
-        cout << "> Nombre: " << nombre << endl;
+        cout << "|---------------------------------------|" << endl;
+
+        cout << "> Grupo: " << getGrupo() << endl;
+        cout << "> Codigo de Barras: " << getCodigoDeBarras()<< endl;
+        cout << "> Nombre: " << getNombre() << endl;
         for (int i = 0; i < cantDepositos; i++) {
-            cout << "> Deposito " << i+1 << ": " << depositos[i] << endl;
+            cout << "> Deposito " << i + 1 << ": " << depositos[i] << endl;
         }
+        cout << "> Stock Total: " << getStockTotal() << endl; // Usar el método obtenerStockTotal
+
         cout << "|---------------------------------------|" << endl;
     }
 
-    const string &getNombre() const {
-        return nombre;
+    // Método para obtener el stock de un artículo en un depósito específico por índice.
+    int obtenerStockDeposito(int indice) const {
+        if (indice >= 0 && indice < depositos.size()) {
+            return depositos[indice];
+        } else {
+            cerr << "ERROR: Índice fuera de rango." << endl;
+            return 0; // Retornar 0 o lanzar una excepción según la lógica de tu aplicación.
+        }
     }
 
 
+    // Obtiene el nombre del artículo.
+    const string& getNombre() const {
+        return nombre;
+    }
 
+    // Obtiene el código de barras del artículo.
+    const string& getCodigoDeBarras() const {
+        return codigoDeBarras;
+    }
+
+    // Obtiene el grupo del artículo.
+    const string &getGrupo() const {
+        return grupo;
+    }
+
+    // Establece el grupo del artículo.
+    void setGrupo(const string &grupo) {
+        this->grupo = grupo;
+    }
+
+    // Establece el código de barras del artículo.
+    void setCodigoDeBarras(const string &codigoDeBarras) {
+        this->codigoDeBarras = codigoDeBarras;
+    }
+
+    // Establece el nombre del artículo.
+    void setNombre(const string &nombre) {
+        this->nombre = nombre;
+    }
+
+    // Obtiene la lista de depósitos donde se encuentra el artículo.
+    const vector<int> &getDepositos() const {
+        return depositos;
+    }
+
+    // Establece la lista de depósitos donde se encuentra el artículo.
+    void setDepositos(const vector<int> &depositos) {
+        this->depositos = depositos;
+    }
 };
-
 
 #endif //PROYECTO_ARTICULO_H
